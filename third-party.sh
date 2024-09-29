334211383734ef3d89e1e7a2f2894bb0b412a279
chanshimudingxi <chanshimudingxi@gmail.com>
2024-09-29 15:14:06 +0800

    改成static编译

--- third-party.sh
+++ third-party.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-WORKPATH=$(dirname $(pwd))
+#WORKPATH=$(dirname $(pwd))
+WORKPATH=$(pwd)
 echo "workpath: ${WORKPATH}"
-THIRD_PARTY_PATH=${WORKPATH}/third-party
-echo "third-party: ${THIRD_PARTY_PATH}"
+THIRD_PARTY_PATH=${WORKPATH}/third_party
+echo "third_party: ${THIRD_PARTY_PATH}"
 BUILD_PATH=${WORKPATH}/build
 echo "build: ${BUILD_PATH}"
 
@@ -29,7 +30,7 @@ else
     make uninstall
     make clean
 fi
-./configure --prefix=/usr/local --static
+./configure --prefix=${BUILD_PATH} --static
 make
 make install
 
@@ -44,32 +45,32 @@ else
     make uninstall
     make clean
 fi
-./config -fPIC no-shared --openssldir=/usr/local/etc/ssl --prefix=/usr/local zlib no-dso
+./config -fPIC no-shared --openssldir=${BUILD_PATH}/etc/ssl --prefix=${BUILD_PATH} zlib no-dso
 make
 make install
 
 # hiredis
 cd ${THIRD_PARTY_PATH}
-if [ ! -d hiredis ]; then
-    git clone git@github.com:redis/hiredis.git
-    cd hiredis
-    git checkout -b v1.2.0 v1.2.0
+if [ ! -d hiredis-1.2.0 ]; then
+    wget https://github.com/redis/hiredis/archive/refs/tags/v1.2.0.tar.gz
+    tar -zxvf v1.2.0.tar.gz
+    cd hiredis-1.2.0
 else
-    cd hiredis
+    cd hiredis-1.2.0
     make uninstall
     make clean
 fi
-make PREFIX=${BUILD_PATH}
+make PREFIX=${BUILD_PATH} static
 make PREFIX=${BUILD_PATH} install
 
 # redis-plus-plus
 cd ${THIRD_PARTY_PATH}
-if [ ! -d redis-plus-plus ]; then
-    git clone git@github.com:sewenew/redis-plus-plus.git
-    cd redis-plus-plus
-    git checkout -b 1.3.12 1.3.12
+if [ ! -d redis-plus-plus-1.3.12 ]; then
+    wget https://github.com/sewenew/redis-plus-plus/archive/refs/tags/1.3.12.tar.gz
+    tar -zxvf 1.3.12.tar.gz
+    cd redis-plus-plus-1.3.12
 else
-    cd redis-plus-plus
+    cd redis-plus-plus-1.3.12
     make uninstall
     make clean
 fi
@@ -77,18 +78,18 @@ if [ ! -d build ]; then
     mkdir -p build
 fi
 cd build
-cmake3 -DCMAKE_PREFIX_PATH=${BUILD_PATH} -DCMAKE_INSTALL_PREFIX=${BUILD_PATH} -DREDIS_PLUS_PLUS_CXX_STANDARD=11 ..
+cmake3 -DCMAKE_PREFIX_PATH=${BUILD_PATH} -DCMAKE_INSTALL_PREFIX=${BUILD_PATH} -DREDIS_PLUS_PLUS_CXX_STANDARD=11 -DREDIS_PLUS_PLUS_BUILD_SHARED=OFF ..
 make
 make install
 
 # curl
 cd ${THIRD_PARTY_PATH}
-if [ ! -d curl ]; then
-    git clone git@github.com:curl/curl.git
-    cd curl
-    git checkout -b curl-8_10_0 curl-8_10_0
+if [ ! -d curl-8_10_0 ]; then
+    wget https://github.com/curl/curl/archive/refs/tags/curl-8_10_0.tar.gz
+    tar -zxvf curl-8_10_0.tar.gz
+    cd curl-8_10_0
 else
-    cd curl
+    cd curl-8_10_0
     make uninstall
     make clean
 fi
@@ -101,8 +102,8 @@ make install
 
 # spdlog
 cd ${THIRD_PARTY_PATH}
-if [ ! -d spdlog ]; then
-    git clone git@github.com:gabime/spdlog.git
-    cd spdlog
-    git checkout -b v1.14.1 v1.14.1
+if [ ! -d spdlog-1.14.1 ]; then
+    wget https://github.com/gabime/spdlog/archive/refs/tags/v1.14.1.tar.gz
+    tar -zxvf v1.14.1.tar.gz
+    cd spdlog-1.14.1
 fi
